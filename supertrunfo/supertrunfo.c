275cb10343b05361cc4e5012bcb30ccc5bef7b05
Maxx0009 <maxsuel_crf2110@hotmail.com>
2025-01-18 22:50:44 +0000

    estados

---
+++ supertrunfo/supertrunfo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int Acre,Alagoas,Amapa,Amazona,Bahia,Ceará,Distrito Federal,Espirito Santo;
+    int Goiás,Maranhao,Mato Grosso,Mato Grosso do Sul,Minas Gerais,Pará,Paraiba;
+    int Pernanbuco,Piauí,Rio de Janeiro,Rio Grande do Norte,Rio Grande do Sul;
+    int Rondônia, Roraima,Santa Catarina,Sao Paulo, Sergipe,Tocantins;
+
+
+
+
+
+
+
+
+
+
+
+}
